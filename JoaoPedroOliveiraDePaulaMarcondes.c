5f3fd5e9b56952eedb649814c5a9202fb3a5be3b
Joao Pedro <joaopopm@usp.br>
2024-06-20 12:08:24 -0300

    Adicionei a remocao e alterei a localizacao do split

--- JoaoPedroOliveiraDePaulaMarcondes.c
+++ JoaoPedroOliveiraDePaulaMarcondes.c
@@ -38,6 +38,16 @@ void mostrarArvoreBMais(NO* p, int i) {
 
 }
 
+void mostrar(ArvoreBMais* arv) {
+    if (arv->raiz->numChaves == 0) {
+        printf("Vazia\n");
+        return;
+    }
+    printf("(");
+    mostrarArvoreBMais(arv->raiz, 0);
+    printf(")\n");
+}
+
 void mostrarDetalhes(NO* p) {
     if (!p) {
         return;
@@ -114,8 +124,8 @@ void split(NO* pai, NO* p, int i) {
     if (p->folha) {
         // arruma NO novo
         int a;
-        for (a = t; a < 2*t-1; a++) {
-            novo->chave[a-t] = p->chave[a];
+        for (a = t-1; a < 2*t-1; a++) {
+            novo->chave[a-t+1] = p->chave[a];
             p->chave[a] = -1;
             p->numChaves--;
             novo->numChaves++;
@@ -232,7 +242,79 @@ void adicionarArvoreBMais(ArvoreBMais* arv, int registro) {
     insereArvoreBMais(p, registro);
 }
 
+void removerNoFolha(NO* p, int registro) {
+    int i = 0;
+    while (i < p->numChaves && registro > p->chave[i]) {
+        i++;
+    }
+
+    if (p->chave[i] == registro) {
+        int a;
+        for (a = i; a < p->numChaves; a++) {
+            p->chave[a] = p->chave[a+1];
+        }
+        p->chave[a] = -1;
+        p->numChaves--;
+    }
+
+}
+
+void merge(NO* pai, NO* p, NO* q, int i) {
+    int a, b;
+    for (a = p->numChaves, b = 0; b < q->numChaves; a++, b++) {
+        p->chave[a] = q->chave[b];
+    }
+    p->numChaves += q->numChaves;
+    p->filhos[2*t-1] = q->filhos[2*t-1];
+
+    //free(q);
+
+    // arruma pai
+    int c;
+    for (c = i; c < pai->numChaves; c++) {
+        pai->chave[c] = pai->chave[c+1];
+        pai->filhos[c+1] = pai->filhos[c+2];
+    }
+    pai->chave[c] = -1;
+    pai->filhos[c] = NULL;
+    pai->numChaves--;
+
+    pai->chave[i-1] = minimoFilhoEsquerdo(pai->filhos[i]);
+}
+
+void removerArv(NO* p, int registro) {
+    if (p->folha) {
+        removerNoFolha(p, registro);
+        return;
+    }
+
+    int i = 0; // indica o filho da direita da chave que quer remover
+    while (registro >= p->chave[i] && i < p->numChaves) {
+        i++;
+    }
+    removerArv(p->filhos[i], registro);
+    p->chave[i-1] = minimoFilhoEsquerdo(p->filhos[i]);
+    NO* filho = p->filhos[i];
+    if (filho->numChaves < t) {
+        if (i > 0) {
+            NO* filho_esquerda = p->filhos[i-1];
+            if (filho_esquerda->numChaves < t) {
+                merge(p, filho_esquerda, filho, i-1);
+            } else {
+                // pegar a chave do lado
+            }
+        }
+    }
+}
+
 void removerArvoreBMais(ArvoreBMais* arv, int registro) {
+    NO* p = arv->raiz;
+
+    removerArv(p, registro);
+
+    if (p->numChaves == 0) {
+        arv->raiz = p->filhos[0];
+    }
 
 }
 
@@ -245,14 +327,24 @@ int main() {
     ArvoreBMais arv;
     inicializarArvoreBMais(&arv);
 
-    adicionarArvoreBMais(&arv, 10);
-    adicionarArvoreBMais(&arv, 15);
-    adicionarArvoreBMais(&arv, 20);
-    adicionarArvoreBMais(&arv, 11);
-    adicionarArvoreBMais(&arv, 25);
+    mostrar(&arv);
+
     adicionarArvoreBMais(&arv, 30);
+
+    mostrar(&arv);
+
     adicionarArvoreBMais(&arv, 9);
-    adicionarArvoreBMais(&arv, 12);
+    adicionarArvoreBMais(&arv, 5);
+    adicionarArvoreBMais(&arv, 8);
+    adicionarArvoreBMais(&arv, 17);
+    adicionarArvoreBMais(&arv, 26);
+
+    mostrar(&arv);
+
+    //adicionarArvoreBMais(&arv, 25);
+    //adicionarArvoreBMais(&arv, 30);
+    //adicionarArvoreBMais(&arv, 9);
+    /*adicionarArvoreBMais(&arv, 12);
     adicionarArvoreBMais(&arv, 8);
     adicionarArvoreBMais(&arv, 13);
     adicionarArvoreBMais(&arv, 23);
@@ -260,13 +352,19 @@ int main() {
     adicionarArvoreBMais(&arv, 13);
     adicionarArvoreBMais(&arv, 7);
     adicionarArvoreBMais(&arv, 16);
-    adicionarArvoreBMais(&arv, 17);
+    adicionarArvoreBMais(&arv, 17); */
     
+    //removerArvoreBMais(&arv, 20);
+    //removerArvoreBMais(&arv, 9);
+    //removerArvoreBMais(&arv, 10);
 
+    //removerArvoreBMais(&arv, 25);
 
-    mostrarArvoreBMais(arv.raiz, 0);
-    printf("\n");
-    printf("\n");
+    //adicionarArvoreBMais(&arv, 12);
+    //adicionarArvoreBMais(&arv, 40);
+
+    //mostrar(&arv);
+    //printf("\n");
     //mostrarDetalhes(arv.raiz);
 
     int qnt = contarRegistros(&arv);
